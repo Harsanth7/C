e7953f26b7e44ef0091a7c5a1a1befe935d33dbb
Harsanth7 <147228724+Harsanth7@users.noreply.github.com>
2023-10-07 18:32:46 +0530

    Add files via upload

---
+++ 3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main() 
+{
+int a=10;
+float b=7.3; 
+char c='m';
+char d[10]="Raja";
+clrscr();
+printf("A=%d",a);
+printf("\nB=%f",b); 
+printf("\nC=%c",c); 
+printf("\nD=%s",d);
+getch();
+}
